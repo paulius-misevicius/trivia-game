3a0d50631f58344d5695c2e79cc6c2a80f340c67
spr0nk6 <paulius.misevicius@protonmail.com>
2026-06-16 15:22:24 +0300

    Add submitable form question with hardcoded data and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+export default function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+  function submitQuiz(formData) {
+    console.log(formData.get("q-1"))
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <main>
+      <form action={submitQuiz}>
+        <section className="question-component">
+          <h2 className="question-name">How would one say goodbye in Spanish?</h2>
+          <div className="question-answer-field">
+            <input type="radio" id="q-1-a-1" name="q-1" value="Adiós"></input>
+            <label htmlFor="q-1-a-1">Adiós</label>
 
-      <div className="ticks"></div>
+            <input type="radio" id="q-1-a-2" name="q-1" value="Hola"></input>
+            <label htmlFor="q-1-a-2">Hola</label>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+            <input type="radio" id="q-1-a-3" name="q-1" value="Au Revoir"></input>
+            <label htmlFor="q-1-a-3">Au Revoir</label>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+            <input type="radio" id="q-1-a-4" name="q-1" value="Salir"></input>
+            <label htmlFor="q-1-a-4">Salir</label>
+          </div>
+        </section>
+        <button className="submit-btn">Submit quiz</button>
+      </form>
+    </main>
   )
 }
-
-export default App
